4af511435853af56c746241f58a2dfcf297c3127
hmr53 <160482647+hmr53@users.noreply.github.com>
2024-02-23 10:03:38 +0530

    Initial Commit

---
+++ restful-webservices-for-socialmedia-application/src/main/java/com/hasith/restfulwebservicesforsocialmediaapplication/RestfulWebservicesForSocialmediaApplication.java
@@ -0,0 +1,13 @@
+package com.hasith.restfulwebservicesforsocialmediaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesForSocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesForSocialmediaApplication.class, args);
+	}
+
+}
---
+++ restful-webservices-for-socialmedia-application/src/test/java/com/hasith/restfulwebservicesforsocialmediaapplication/RestfulWebservicesForSocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasith.restfulwebservicesforsocialmediaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebservicesForSocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
