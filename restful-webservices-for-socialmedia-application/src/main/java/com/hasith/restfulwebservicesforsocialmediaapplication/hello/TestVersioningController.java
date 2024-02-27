d945a31a35e055ed4e61910eea40c8ceeb353c03
hmr53 <h.malshan.53@gmail.com>
2024-02-27 10:54:46 +0530

    Versioning Demo. Using Custom Headers. (X-API-VERSION)

--- restful-webservices-for-socialmedia-application/src/main/java/com/hasith/restfulwebservicesforsocialmediaapplication/hello/TestVersioningController.java
+++ restful-webservices-for-socialmedia-application/src/main/java/com/hasith/restfulwebservicesforsocialmediaapplication/hello/TestVersioningController.java
@@ -6,22 +6,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestVersioningController {
 
-    @GetMapping("/v1/person")
-    public PersonV1 sendPersonV1(){
-        return new PersonV1("Hasith Malshan");
-    }
+//    @GetMapping("/v1/person")
+//    public PersonV1 sendPersonV1(){
+//        return new PersonV1("Hasith Malshan");
+//    }
+//
+//    @GetMapping("/v2/person")
+//    public PersonV2 sendPersonV2(){
+//        return new PersonV2("Hasith","Malshan");
+//    }
+//
+//    @GetMapping(value = "/person", params = "version=1")
+//    public PersonV1 sendPersonV1Param(){
+//        return new PersonV1("Hasith Malshan");
+//    }
+//
+//    @GetMapping(value = "/person",params = "version=2")
+//    public PersonV2 sendPersonV2Param(){
+//        return new PersonV2("Hasith","Malshan");
+//    }
 
-    @GetMapping("/v2/person")
-    public PersonV2 sendPersonV2(){
-        return new PersonV2("Hasith","Malshan");
-    }
-
-    @GetMapping(value = "/person", params = "version=1")
+    @GetMapping(value = "/person", headers = "X-API-VERSION=1")
     public PersonV1 sendPersonV1Param(){
         return new PersonV1("Hasith Malshan");
     }
 
-    @GetMapping(value = "/person",params = "version=2")
+    @GetMapping(value = "/person",headers = "X-API-VERSION=2")
     public PersonV2 sendPersonV2Param(){
         return new PersonV2("Hasith","Malshan");
     }
